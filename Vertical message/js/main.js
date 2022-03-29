81a24447f522457b92fd7d89219e84f8c524a805
RD1N <64215825+RD1N@users.noreply.github.com>
2022-03-29 10:29:40 +0300

    Add files via upload

---
+++ Vertical message/js/main.js
@@ -0,0 +1,10 @@
+function showVerticalMessage( word ) {
+    const ord = word[1] + word[2] + word[3] + word[4] + word[5] + word[6]
+    let str = word[0].toUpperCase() + ord;
+    for (let char of str) {
+        console.log(char)
+    };
+
+};
+
+showVerticalMessage('марафон')
