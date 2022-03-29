ec1953c13cc592e00659197612453aff4396517f
RD1N <64215825+RD1N@users.noreply.github.com>
2022-03-29 11:10:13 +0300

    Update main.js

--- Vertical message/js/main.js
+++ Vertical message/js/main.js
@@ -1,6 +1,11 @@
 function showVerticalMessage( word ) {
-    const ord = word[1] + word[2] + word[3] + word[4] + word[5] + word[6]
-    let str = word[0].toUpperCase() + ord;
+    let str;
+    let ord = word[1] + word[2] + word[3] + word[4] + word[5] + word[6];
+    if (word[0] === "p") {
+        str = word[0].toUpperCase() + ord;
+    } else {
+        str = word;
+    }
     for (let char of str) {
         console.log(char)
     };
